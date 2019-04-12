f1939703fa11b24ada35de68650ff60db61d33dd
JEverett <jonathan@jonathaneverettcreative.com>
2019-04-12 11:04:27 -0500

    Fixed error in database reference. Created functional display of all products upon loading. Beginning inquirer prompts to take purchase input from customers.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,11 +12,42 @@ var connection = mysql.createConnection({
     user: "root",
 
     password: login.password,
-    database: "products"
+    database: "bamazon"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected as id: " + connection.threadId + "\n");
-
+    displayAvailable();
 });
+
+function displayAvailable() {
+    console.log("Showing all available products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        for(var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + "\n"
+            + "Product Name: " + res[i].product_name + "\n"
+            + "Price: " + res[i].price + "\n")
+        }
+        selectPurchase();
+    });
+}
+
+function selectPurchase() {
+    inquirer
+        .prompt({
+            name: "choice",
+            type: "input",
+            message: "Enter the Item ID number of the product you would like to order."
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many units would you like to order? (Please enter the numeric value."
+
+        }).then(function(answer) {
+            
+        })
+}
