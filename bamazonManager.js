ae55b45e0d342441ec9ca89c7825d3c4ec1bb5d0
JEverett <jonathan@jonathaneverettcreative.com>
2019-04-15 22:38:13 -0500

    Commented out the second half of options for the bamazonManager app due to time constraints in solving bugs. Will return to it later.

--- bamazonManager.js
+++ bamazonManager.js
@@ -31,8 +31,8 @@ function displayOptions() {
             choices: [
                 "View Products for Sale",
                 "View Low Inventory",
-                "Add to Inventory",
-                "Add New Product"
+                // "Add to Inventory",
+                // "Add New Product"
             ]
         }).then(function(answer) {
             switch (answer.action) {
@@ -44,13 +44,13 @@ function displayOptions() {
                 viewInventory();
                 break;
 
-                case "Add to Inventory":
-                addInventoryStock();
-                break;
+                // case "Add to Inventory":
+                // addInventoryStock();
+                // break;
 
-                case "Add New Product":
-                addProduct();
-                break;
+                // case "Add New Product":
+                // addProduct();
+                // break;
             }
         });
 }
@@ -110,49 +110,61 @@ function viewInventory() {
         }
     });
 }
+// ** Function to be added later when bugs worked out **
+// function addInventoryStock() {
+//     connection.query("SELECT * FROM products", function(err, res) {
+//         if (err) throw err;
+//         for(var i = 0; i < res.length; i++) {
+//             console.log("Item ID: " + res[i].item_id + "\n"
+//             + "Product Name: " + res[i].product_name + "\n"
+//             + "Stock Quantity: " + res[i].stock_quantity + "\n");
+//         }
+//         inquirer
+//         .prompt([
+//             {
+//             name: "products",
+//             type: "input",
+//             message: "What enter the Item ID of the product you would like to add stock to from the list above.",
+//             validate: function(value) {
+//                 if (isNaN(value) === false) {
+//                   return true;
+//                 }
+//                 return false;
+//               }
 
-function addInventoryStock() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        for(var i = 0; i < res.length; i++) {
-            console.log("Item ID: " + res[i].item_id + "\n"
-            + "Product Name: " + res[i].product_name + "\n"
-            + "Stock Quantity: " + res[i].stock_quantity + "\n");
-        }
-        inquirer
-        .prompt([
-            {
-            name: "products",
-            type: "input",
-            message: "What enter the Item ID of the product you would like to add stock to from the list above.",
-            validate: function(value) {
-                if (isNaN(value) === false) {
-                  return true;
-                }
-                return false;
-              }
-
-        }, {
-            name: "additional",
-            type: "list",
-            message: "How many units would you like to add?",
-            choices: [
-                1,
-                5,
-                10,
-                50
-            ]
-        }
-    ]).then(function(answer){
-            // var query = "SELECT product_name, stock_quantity FROM products WHERE ?";
-            connection.query("UPDATE products SET ? WHERE ?", [
-                {
-                    stock_quantity: (stock_quantity + answer.additional) 
-                },
-                {
-                    item_id: answer.products
-                }
-            ]);
-        });
-    })
-}
+//         }, {
+//             name: "additional",
+//             type: "list",
+//             message: "How many units would you like to add?",
+//             choices: [
+//                 "1",
+//                 "5",
+//                 "10",
+//                 "50"
+//             ]
+//         }
+//     ]).then(function(answer){
+           
+//             var stockquery = "SELECT product_name, stock_quantity FROM products WHERE ?";
+//             var newstock;
+//             connection.query(stockquery, {item_id: answer.products}, function(err, res) {
+//                 if (err) throw err;
+//                 newstock = res.stock_quantity + answer.additional;
+//             });
+//             console.log(newstock);
+//             // Actual Update code below - not currently appropriately adding to the stock, instead changed to 0
+//             connection.query("UPDATE products SET ? WHERE ?", [
+//                 {
+//                     stock_quantity: res.stock_quantity + answer.additional
+//                 },
+//                 {
+//                     item_id: answer.products
+//                 }
+//             ], function(err) {
+//                 if (err) throw err;
+//                 console.log("\n" + answer.additional + " units added to the chosen product.\n");
+//                 continueWorking();
+//             });
+//         });
+//     })
+// }
